067d79ed1393f13c8b1701735e841f2b680843b2
이종원 <ijong-won@ijong-won-ui-MacBookPro.local>
2020-12-28 00:19:28 +0900

    finshed mypage and EditInfo

--- my-app/src/component/Field.js
+++ my-app/src/component/Field.js
@@ -69,6 +69,7 @@ class Field extends React.Component {
 
     this.renderFieldContents = this.renderFieldContents.bind(this);
     this.potatoRenderHandler = this.potatoRenderHandler.bind(this);
+    this.rennderPotatos = this.renderPotatos.bind(this);
   }
 
   potatoRenderHandler() {
@@ -77,16 +78,49 @@ class Field extends React.Component {
     });
   }
 
+  // 밭들을 렌더하는 메소드
   renderFieldContents() {
-    return this.state.data.fields.map((f) => (
-      <FieldContents
-        potatoRenderHandler={this.potatoRenderHandler}
-        fieldName={f.fieldName}
-        fieldDesc={f.fieldDesc}
-        potatoCount={this.state.data.potatoes.length}
-        category={f.category}
-      />
-    ));
+    return (
+      <div className="field_right_fields">
+        <div className="field_right_bar">
+          <button className="field_right_field_add_btn">밭 추가</button>
+          <button className="field_right_field_remove_btn">밭 삭제</button>
+        </div>
+
+        <div className="field_right_field_contents">
+          {this.state.data.fields.map((f) => (
+            <FieldContents
+              potatoRenderHandler={this.potatoRenderHandler}
+              fieldName={f.fieldName}
+              fieldDesc={f.fieldDesc}
+              potatoCount={this.state.data.potatoes.length}
+              category={f.category}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+
+  // 감자들을 렌더하는 메소드.
+  renderPotatos() {
+    return (
+      <div className="field_right_fields">
+        <div className="field_right_bar">
+          <button className="field_right_potato_add_btn">감자 추가</button>
+          <button className="field_right_potato_remove_btn">감자 삭제</button>
+          <button className="field_right_potato_show-all_btn">모든 감자</button>
+          <button className="field_right_potato_show-bad_btn">썪은 감자</button>
+        </div>
+
+        <div className="field_right_potato_contents">
+          <div className="potato_component">
+            <Potato />
+            <Potato />
+          </div>
+        </div>
+      </div>
+    );
   }
 
   render() {
@@ -122,20 +156,11 @@ class Field extends React.Component {
             <div className="field_right_menu_manual">사용 설명서</div>
             <div className="field_right_menu_logout">로그아웃</div>
           </div>
-          <div className="field_right_fields">
-            <div className="field_right_bar">
-              <button className="field_right_add_btn">추가</button>
-              <button className="field_right_remove_btn">삭제</button>
-            </div>
 
-            <div className="field_right_contents">
-              {!this.state.isFieldClicked ? (
-                this.renderFieldContents()
-              ) : (
-                <Potato />
-              )}
-            </div>
-          </div>
+          {/* 밭을 보여주느냐, 감자를 보여주느냐 결정하는 부분 */}
+          {!this.state.isFieldClicked
+            ? this.renderFieldContents()
+            : this.renderPotatos()}
         </div>
       </div>
     );
